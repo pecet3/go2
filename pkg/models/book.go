786272b2e866bd1e957ff49272fcef6bc09f3e02
pecet3 <pecet3107@gmail.com>
2023-10-22 01:59:12 +0200

    testing...gorm...

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	"github.com/pecet3/go2/pkg/config"
 )
@@ -9,12 +11,45 @@ var db *gorm.DB
 
 type Book struct {
 	gorm.Model
-	Name        string `gorm:""json:"name"`
+	Name        string `gorm:"" json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
 
 func initial() {
+	fmt.Println("intilal")
 	config.Connect()
+	fmt.Println("intilal")
 	db = config.GetDB()
+	if db == nil {
+		fmt.Print("errrrrrrr")
+	}
+	fmt.Println("intilal")
+	db.AutoMigrate(&Book{})
+	fmt.Println("intilal")
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	// db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+
+	return &getBook, db
+}
+
+func DeleteBookById(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
 }
