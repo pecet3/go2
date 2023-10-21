44a64331e71d497deb0ec8337e12b451bdb40a4b
pecet3 <pecet3107@gmail.com>
2023-10-22 01:58:41 +0200

    .

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,10 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"log"
-	"os"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 )
 
@@ -21,17 +22,18 @@ var (
 )
 
 func Connect() {
-	dbPassword := os.Getenv("DB_PASSWORD")
+	dbPassword := ""
 	dsn := "user:" + dbPassword + "@tcp(jakubpacewi.cz:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
 	d, err := gorm.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error with db")
 	}
-
+	fmt.Println(d)
 	db = d
 
 }
 
 func GetDB() *gorm.DB {
+	fmt.Println(db)
 	return db
 }
