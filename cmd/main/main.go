a82b8ebdcfc9c1d5a0e81ab99bfb2b6fc52fba51
pecet3 <pecet3107@gmail.com>
2023-10-23 23:33:52 +0200

    .

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,66 +1,30 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-
-// 	"github.com/gorilla/mux"
-// 	"github.com/pecet3/go2/pkg/routes"
-// )
-
-// func main() {
-// 	fmt.Printf("Listening")
-// 	r := mux.NewRouter()
-// 	routes.RegisterBookStoreRoutes(r)
-// 	fmt.Printf("Listening 2")
-// 	http.Handle("/", r)
-// 	fmt.Printf("Listening 3")
-
-// 	if err := http.ListenAndServe("localhost:8080", r); err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Printf("Listening 4")
-
-// }
-
 package main
 
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/joho/godotenv"
-)
-
-func initial() {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		log.Println("error")
-	}
-
-}
-
-var (
-	db *gorm.DB
+	"github.com/gorilla/mux"
+	"github.com/pecet3/go2/pkg/models"
+	"github.com/pecet3/go2/pkg/routes"
 )
 
 func main() {
-	dbPassword := "Reksiokuba1!"
-	dsn := "user:" + dbPassword + "@tcp(jakubpacewi.cz:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
-	d, err := gorm.Open("mysql", dsn)
-	if err != nil {
-		fmt.Printf("Error with db")
-	}
-	fmt.Println(d)
-	db = d
-	fmt.Println(db)
-}
 
-func GetDB() *gorm.DB {
-	fmt.Println(db)
-	return db
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+
+	port := 8080
+	models.Initial()
+	fmt.Printf("Starting server at port:%v\n", port)
+
+	servePort := ":" + strconv.Itoa(port)
+
+	if err := http.ListenAndServe(servePort, r); err != nil {
+		log.Fatal(err)
+	}
 }
